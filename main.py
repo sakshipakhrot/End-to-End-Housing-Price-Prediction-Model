2bda21f984bb2f690defae4cda51dd6dad7d0bd0
sakshipakhrot <sakshipakhrot16@gmail.com>
2026-02-15 16:36:21 +0530

    Final Updated Files

--- main.py
+++ main.py
@@ -4,12 +4,47 @@ from src.datascience.pipeline.data_transformation_pipeline import DataTransforma
 from src.datascience.pipeline.data_preprocessing_pipeline import DataPreProcessingTrainingPipeline
 from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerPipeline
 
-STAGE_NAME="Data Ingestion Stage"
+
+
+STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f"-------------> stage {STAGE_NAME} started <------------------")
-    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.initiate_data_ingestion()
-    logger.info(f"-------------> stage {STAGE_NAME} completed <------------------")
+    logger.info(f"-------------> stage {STAGE_NAME} completed <------------------\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f"-------------> stage {STAGE_NAME} started <------------------")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.initiate_data_transformation()
+    logger.info(f"-------------> stage {STAGE_NAME} completed <------------------\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Preprocessing Stage"
+try:
+    logger.info(f"-------------> stage {STAGE_NAME} started <------------------")
+    data_preprocessing = DataPreProcessingTrainingPipeline()
+    data_preprocessing.initiate_data_preprocessing()
+    logger.info(f"-------------> stage {STAGE_NAME} completed <------------------\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer Stage"
+try:
+    logger.info(f"-------------> stage {STAGE_NAME} started <------------------")
+    model_trainer = ModelTrainerPipeline()
+    model_trainer.initiate_model_trainer()
+    logger.info(f"-------------> stage {STAGE_NAME} completed <------------------\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
